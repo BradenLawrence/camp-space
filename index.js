c647c6808d9060c8a208b98003401e007710b1ee
Braden Lawrence <lawrencebradenm@gmail.com>
2017-03-29 23:11:45 +0000

    Adding header and footer partials and post route to add new campgrounds

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 var express = require("express")
 var app = express()
+var bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 app.use(express.static('assets'))
 
-app.get("/", function(request, response) {
-    response.render("home")
-})
-
-app.get("/campgrounds", function(request, response) {
-    var sites = [
+var sites = [
         {name: "Yellowknife Bay", location: "Mars", img: "img/yellowknifeBay.jpg"},
         {name: "Cupola Module", location: "International Space Station", img: "img/cupola.jpg"}, 
         {name: "Bonneville Crater", location: "Mars", img: "img/bonnevilleCrater.jpg"}, 
         {name: "Double Crater", location: "Moon", img: "img/apollo11Flag.jpg"}, 
         {name: "Huygen's Landing", location: "Titan", img: "img/huygens.jpg"}, 
     ]
+
+app.get("/", function(request, response) {
+    response.render("home")
+})
+
+app.get("/campgrounds", function(request, response) {
+    
     response.render("campgrounds", {sites: sites})
+})
+
+app.post("/campgrounds", function(request, response) {
+    var name = request.body.name
+    var loc = request.body.loc
+    var img = request.body.img
+    sites.push({name: name, location: loc, img: img})
+    response.redirect("/campgrounds")
+})
+
+app.get("/campgrounds/new", function(request, response) {
+    response.render("new.ejs")
 })
 
 app.listen(process.env.PORT, process.env.IP, function() {
