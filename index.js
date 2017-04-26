c2543e88e19bdb2dbc733a70e4c558a1494c734c
Braden Lawrence <lawrencebradenm@gmail.com>
2017-04-26 20:31:26 +0000

    Removed the sites array in index.js and replaced it with Mongo DB.
    - Landing sites are now retrieved from the database rather than a local array.
    - app.post now stores new sites in the database, these now persist after restarting the server.

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
 var express = require("express")
 var app = express()
 var bodyParser = require("body-parser")
-
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 app.use(express.static('assets'))
+var mongoose = require("mongoose")
+mongoose.Promise = global.Promise
+mongoose.connect("mongodb://localhost/camp-space")
 
-var sites = [
-        {name: "Yellowknife Bay", location: "Mars", img: "img/yellowknifeBay.jpg"},
-        {name: "Cupola Module", location: "International Space Station", img: "img/cupola.jpg"}, 
-        {name: "Bonneville Crater", location: "Mars", img: "img/bonnevilleCrater.jpg"}, 
-        {name: "Double Crater", location: "Moon", img: "img/apollo11Flag.jpg"}, 
-        {name: "Huygen's Landing", location: "Titan", img: "img/huygens.jpg"},
-        {name: "Yellowknife Bay", location: "Mars", img: "img/yellowknifeBay.jpg"},
-        {name: "Cupola Module", location: "International Space Station", img: "img/cupola.jpg"}, 
-        {name: "Bonneville Crater", location: "Mars", img: "img/bonnevilleCrater.jpg"}, 
-        {name: "Double Crater", location: "Moon", img: "img/apollo11Flag.jpg"}, 
-        {name: "Huygen's Landing", location: "Titan", img: "img/huygens.jpg"},
-        {name: "Yellowknife Bay", location: "Mars", img: "img/yellowknifeBay.jpg"},
-        {name: "Cupola Module", location: "International Space Station", img: "img/cupola.jpg"}, 
-        {name: "Bonneville Crater", location: "Mars", img: "img/bonnevilleCrater.jpg"}, 
-        {name: "Double Crater", location: "Moon", img: "img/apollo11Flag.jpg"}, 
-        {name: "Huygen's Landing", location: "Titan", img: "img/huygens.jpg"}
-    ]
+var siteSchema = new mongoose.Schema({
+    name: String,
+    location: String,
+    img: String
+})
+
+var Site = mongoose.model("Site", siteSchema)
 
 app.get("/", function(request, response) {
     response.render("home")
 })
 
 app.get("/landingSites", function(request, response) {
-    
-    response.render("landingSites", {sites: sites})
+    Site.find({}, function(error, dbResponse){
+        if(error){
+            console.log("Something went wrong, could not find sites.")
+            console.log(error)
+        } else {
+            response.render("landingSites", {sites: dbResponse})
+            console.log(dbResponse)
+        }
+    })
 })
 
 app.post("/landingSites", function(request, response) {
-    var name = request.body.name
-    var loc = request.body.loc
-    var img = request.body.img
-    sites.push({name: name, location: loc, img: img})
-    response.redirect("/landingSites")
+    Site.create({
+        name: request.body.name,
+        location: request.body.loc,
+        img: request.body.img
+    }), function(error, newSite){
+        if(error){
+            console.log("Something went wrong, could not add site.")
+            console.log(error)
+        } else {
+            console.log("Added new entry to Site")
+            console.log(newSite)
+            response.redirect("/landingSites")
+        }
+    }
+    
 })
 
 app.get("/landingSites/new", function(request, response) {
