9ed537f93658b3fa1454490e7bc8d8b64156b3b2
Braden Lawrence <lawrencebradenm@gmail.com>
2017-05-20 21:57:00 +0000

    Adding a user signup route - Adding session management and auth management packages   - express-session   - passport, passport-local, and passport-local-mongoose - Creating a new User model in the database - Adding settings for passport and organizing app settings - Adding get route for /register - Creating a view for the register page with a signup form - Adding a post route for /register, currently does not handle the form data

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    Site        = require("./models/landingsite"),
-    Comment     = require("./models/comment"),
-    seedDB      = require("./seeds")
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    expressSession  = require("express-session"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    // MODELS
+    User            = require("./models/user"),
+    Site            = require("./models/landingsite"),
+    Comment         = require("./models/comment"),
+    seedDB          = require("./seeds")
     
 seedDB()    
 
+// GENERAL APP SETTINGS
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/assets"))
+
+// MONGOOSE DATABASE SETTINGS
 mongoose.Promise = global.Promise
 mongoose.connect("mongodb://localhost/camp-space")
 
+// PASSPORT AUTHORIZATION SETTINGS
+app.use(expressSession({
+    secret: "Jebediah",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+
+// --------------------------------- //
+// ============= ROUTES ============ //
+// --------------------------------- //
 
 // INDEX
 app.get("/", function(request, response) {
@@ -71,7 +95,6 @@ app.get("/landingSites/:id", function(request, response){
 // COMMENT ROUTES
 // ================================//
 
-
 // COMMENT NEW
 app.get("/landingSites/:id/comments/new", function(request, response){
     Site.findById(request.params.id, function(error, foundSite){
@@ -103,8 +126,21 @@ app.post("/landingsites/:id/comments", function(request, response){
     })
 })
 
+// AUTHENTICATION ROUTES
+// ================================//
+
+// USER NEW
+app.get("/register", function(request, response) {
+    response.render("auth/register")
+})
+
+// USER CREATE
+app.post("/register", function(request, response) {
+    response.send("You're totally signed up! (not really...)")
+})
 
 
+// LISTEN
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("CampSpace server is running...")
 })
