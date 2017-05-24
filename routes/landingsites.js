372fad91e526dc507be89fd3cd578559bef893ec
Braden Lawrence <lawrencebradenm@gmail.com>
2017-05-24 03:38:32 +0000

    Pulling authorization logic into its own middleware - Refactored the EDIT route back to its original logic - Created middleware function isSiteAuthor to compare a site's author   with the current user - Replaced calls to "isLoggedIn" with "isSiteAuthor" on the UPDATE   and DESTROY routes

--- routes/landingsites.js
+++ routes/landingsites.js
@@ -12,7 +12,27 @@ function isLoggedIn(request, response, next){
     if(request.isAuthenticated()){
         return next()
     } else {
-        response.redirect("/login")
+        response.redirect("back")
+    }
+}
+
+// Check if the user posted a certain campground
+function isSiteAuthor(request, response, next){
+    if(request.isAuthenticated()){
+        Site.findById(request.params.id, function(error, foundSite){
+            if(error){
+                console.log(error)
+                response.redirect("/landingsites/" + request.params.id)
+            } else {
+                if(foundSite.author.id.equals(request.user._id)){
+                    next()
+                } else {
+                    response.redirect("back")
+                }
+            }
+        })
+    } else {
+        response.redirect("back")
     }
 }
 
@@ -62,27 +82,19 @@ router.get("/:id", function(request, response){
 })
 
 // EDIT
-router.get("/:id/edit", function(request, response) {
-    if(request.isAuthenticated()){
+router.get("/:id/edit", isSiteAuthor, function(request, response) {
         Site.findById(request.params.id, function(error, foundSite){
             if(error){
                 response.redirect("/landingsites/" + request.params.id)
             } else {
-                if(foundSite.author.id.equals(request.user._id)){
-                    response.render("landingsites/edit", {site: foundSite})
-                } else {
-                    response.redirect("/landingsites/" + request.params.id)
-                }
+                response.render("landingsites/edit", {site: foundSite})
             }
         })
-    } else {
-        response.redirect("/landingsites/" + request.params.id)
-    }
 })
 
 
 // UPDATE
-router.put("/:id", isLoggedIn, function(request, response){
+router.put("/:id", isSiteAuthor, function(request, response){
     Site.findByIdAndUpdate(
         request.params.id, 
         request.body.editSite, 
@@ -98,7 +110,7 @@ router.put("/:id", isLoggedIn, function(request, response){
 })
 
 // DESTROY
-router.delete("/:id", isLoggedIn, function(request, response){
+router.delete("/:id", isSiteAuthor, function(request, response){
     Site.findByIdAndRemove(request.params.id, function(error){
         if(error){
             console.log(error)
